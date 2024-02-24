06ef730e76d3f94c8f18ffa4a731781184536fa5
urke90 <urosbijelic90@gmail.com>
2024-02-24 16:10:24 +0100

    header component with title and logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,5 @@
+import { MdSunnySnowing } from 'react-icons/md';
+
 import './Header.scss';
 
 // ----------------------------------------------------------------
@@ -6,7 +8,10 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <h1 className="header__title">Weater Forcast App</h1>
+        <h1 className="header__title">Weather Forcast App</h1>
+        <div className="header__logo">
+          <MdSunnySnowing size={50} />
+        </div>
       </div>
     </header>
   );
